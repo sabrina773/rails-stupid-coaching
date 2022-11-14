79a72eecd298e9b53fccc40db2a78633dd77ed56
Sabrina Ahamed <tazrian.zawad14@gmail.com>
2022-11-14 16:09:12 +0100

    Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def question
+  end
+
+  def answer
+    @question = params[:input]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+     @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
